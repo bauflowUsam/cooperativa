5aee10de9604430478c6a79f2e9176c7009c86d0
marco.castrousam <marco.castrousam@CDS2-USAM055.sv.cds>
2019-05-08 10:58:19 -0600

    numeroDocumento de persona

--- cooperativa2Prueba/src/com/empresa/modelos/Persona.java
+++ cooperativa2Prueba/src/com/empresa/modelos/Persona.java
@@ -3,6 +3,7 @@ package com.empresa.modelos;
 public class Persona {
 private String nombre;
 private String apellido;
+private String numeroDocumento;
 
 
 public String getNombre() {
@@ -23,6 +24,16 @@ public String getApellido() {
 }
 public void setApellido(String apellido) {
 	this.apellido = apellido;
+}
+
+
+public String getNumeroDocumento() {
+	return numeroDocumento;
+}
+
+
+public void setNumeroDocumento(String numeroDocumento) {
+	this.numeroDocumento = numeroDocumento;
 }
 
 
